515753c88f9b83ce8ccd2fb0f0a1d6b246c175d9
Yomaira Escano <yjescano@gmail.com>
2018-07-27 04:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,8 @@ function removeLastDriver() {
   const copyOfDrivers = drivers.slice(0, drivers.length - 1);
   return copyOfDrivers
 }
+
+function removeFirstDriver() {
+  const copyOfDrivers = drivers.slice(1);
+  return copyOfDrivers
+}
