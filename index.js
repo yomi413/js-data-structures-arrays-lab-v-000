007fea319bac042160ee0dc372f7023a7ff29da8
Yomaira Escano <yjescano@gmail.com>
2018-07-27 04:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function destructivelyAppendDriver(name) {
 function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
